b2222f7bcff7f629ee18e34f0757cb82cc21e766
hamidjany <104394236+hamidjany@users.noreply.github.com>
2022-12-28 19:03:29 +0330

    first setion

---
+++ 01.py
@@ -0,0 +1,17 @@
+# finst game with python
+# date: 28/12/2022
+
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title('hamid game')
+wn.bgcolor('black')
+wn.setup(with=800, height=600)
+wn.tracer(0)
+
+
+# main loop
+while True:
+    
