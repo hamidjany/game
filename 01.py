75b245d5fb8c7c700d47b1c498a1200b22854de5
hamidjany <104394236+hamidjany@users.noreply.github.com>
2023-01-01 16:54:13 +0330

    adding ball

--- 01.py
+++ 01.py
@@ -38,9 +38,65 @@ ball.shape('square')
 ball.color('white')
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
+
+# function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
 
 
-
+# keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, 'w')
+wn.onkeypress(paddle_a_down, 's')
+wn.onkeypress(paddle_b_up, 'Up')
+wn.onkeypress(paddle_b_down, 'Down')
+    
 # main loop
 while True:
     wn.update()
+    
+    # ball movement
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+
+    # border checking
+    if ball.ycor() > 290 :
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290 :
+        ball.sety(-290)
+        ball.dy *= -1
+    
+    if ball.xcor() > 390 :
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+    if ball.xcor() < -390 :
+        ball.goto(0, 0)
+        ball.dx *= -1
+    
+
+    # ball and paddle collision
+    if (ball.xcor() > 340 and ball.xcor() < 350 ) and (paddle_b.ycor() < ball.ycor() + 40 and paddle_b.ycor() > ball.ycor() + 40) :
+        ball.dx *= -1
